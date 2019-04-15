f326f34e8aa87a0aea691680f7bd2d51ef00cdc7
tlockhart <tony.lockhart@ymail.com>
2019-04-15 00:34:25 -0400

    corrected sample data in index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ console.log(myMult1.multiply(1, 3));
 const myMult2 = new Mult();
 console.log(myMult2.multiply(-4, 0));
 
-// const myMult2 = new Mult();
-// console.log(myMult2.multiply(5.9, 3.34));
+// const myMult3 = new Mult();
+// console.log(myMult3.multiply(5.9, 3.34));
